ca698de7c4bb101d93c7e56c3f85cfd7b50cf35d
kaldan007 <kaldantenzin@gmail>
2022-03-14 18:27:34 +0530

    data sampling update

--- get_data_sample.py
+++ get_data_sample.py
@@ -21,19 +21,16 @@ def get_syls(text):
     return syls
 
 def get_context(chunk, type_):
+    chunk = chunk.replace(':', '')
     context = ''
-    if ':' in chunk and type_ == 'left':
-        context = re.search(':(.*)', chunk).group(1)
-
-    else:
-        syls = get_syls(chunk)
-        if len(syls) >= 4:
-            if type_ == 'left':
-                context = f"{''.join(syls[-4:])}"
-            else:
-                context = f"{''.join(syls[:4])}"
+    syls = get_syls(chunk)
+    if len(syls) >= 4:
+        if type_ == 'left':
+            context = f"{''.join(syls[-4:])}"
         else:
-            context = chunk
+            context = f"{''.join(syls[:4])}"
+    else:
+        context = chunk
     return context.strip()
 
 def clean_note(note_text):
@@ -42,13 +39,65 @@ def clean_note(note_text):
         note_text = re.sub(noise_ann, '', note_text)
     return note_text
 
+def get_default_option(prev_chunk):
+    default_option = ''
+    if ':' in prev_chunk:
+        default_option = re.search(':(.*)', prev_chunk).group(1)
+    else:
+        syls = get_syls(prev_chunk)
+        if syls:
+            default_option = syls[-1]
+    return default_option
+
+def get_note_options(default_option, chunk):
+    chunk = re.sub('\(\d+\)', '', chunk)
+    pub_mapping = {
+        '«པེ་»': 'peking',
+        '«པེ»': 'peking',
+        '«སྣར་»': 'narthang',
+        '«སྣར»': 'narthang',
+        '«སྡེ་»': 'derge',
+        '«སྡེ»': 'derge',
+        '«ཅོ་»': 'chone',
+        '«ཅོ»': 'chone'
+    }
+    note_options = {
+        'peking': '',
+        'narthang': '',
+        'derge': '',
+        'chone': ''
+    }
+    note_parts = re.split('(«པེ་»|«སྣར་»|«སྡེ་»|«ཅོ་»|«པེ»|«སྣར»|«སྡེ»|«ཅོ»)', chunk)
+    pubs = note_parts[1::2]
+    notes = note_parts[2::2]
+    for walker, (pub, note_part) in enumerate(zip(pubs, notes)):
+        if note_part:
+            note_options[pub_mapping[pub]] = note_part.replace('>', '')
+        else:
+            if notes[walker+1]:
+                note_options[pub_mapping[pub]] = notes[walker+1].replace('>', '')
+            else:
+                note_options[pub_mapping[pub]] = notes[walker+2].replace('>', '')
+    if not note_options.get('derge', ''):
+        note_options['derge'] = default_option
+    if not note_options.get('narthang', ''):
+        note_options['narthang'] = default_option
+    if not note_options.get('peking', ''):
+        note_options['peking'] = default_option
+    if not note_options.get('chone', ''):
+        note_options['chone'] = default_option
+    return note_options
+
 def get_note_sample(prev_chunk, chunk, next_chunk):
     note_sample = ''
+    default_option = get_default_option(prev_chunk)
+    prev_chunk = re.sub(f'{default_option}$', '', prev_chunk)
     prev_context = get_context(prev_chunk, type_= 'left')
     next_context = get_context(next_chunk, type_= 'right')
-    note = clean_note(chunk)
-    note_sample = f'{prev_context}{note}{next_context}'
-    return note_sample
+    note_options = get_note_options(default_option, chunk)
+    note_options = dict(sorted(note_options.items()))
+    note_sample = f'{prev_context}[{",".join(str(note) for note in note_options.values())}]{next_context}'
+    return [note_sample, note_options]
 
 def parse_notes(collated_text):
     cur_text_notes = []
@@ -60,8 +109,8 @@ def parse_notes(collated_text):
         except:
             next_chunk = ''
         if re.search('\(\d+\) <.+?>', chunk):
-            note_sample = get_note_sample(prev_chunk, chunk, next_chunk)
-            cur_text_notes.append(note_sample)
+            note_sample, note_options  = get_note_sample(prev_chunk, chunk, next_chunk)
+            cur_text_notes.append([note_sample, note_options])
             continue
         prev_chunk = chunk
     return cur_text_notes
@@ -70,7 +119,7 @@ def get_notes_samples(collated_text, note_samples, text_id):
     collated_text = collated_text.replace('\n', '')
     collated_text = re.sub('\d+-\d+', '', collated_text)
     cur_text_notes = parse_notes(collated_text)
-    for cur_text_note in cur_text_notes:
+    for cur_text_note, note_options in cur_text_notes:
         if note_samples.get(cur_text_note, {}):
             note_samples[cur_text_note]['count'] += 1
             note_samples[cur_text_note]['text_id']=text_id
@@ -78,16 +127,53 @@ def get_notes_samples(collated_text, note_samples, text_id):
             note_samples[cur_text_note] = DefaultDict()
             note_samples[cur_text_note]['count'] = 1
             note_samples[cur_text_note]['text_id']=text_id
+            note_samples[cur_text_note]['note_options'] = note_options
     return note_samples
+
+def is_all_option_same(note_options):
+    if note_options['derge'] == note_options['chone'] == note_options['peking'] == note_options['narthang']:
+        return True
+    else:
+        return False
+
+def get_note_context(note):
+    right_context = ''
+    left_context = ''
+    if re.search(r'(.+)\[', note):
+        left_context = re.search(r'(.+)\[', note).group(1)
+    if re.search(r'\](.+)', note):
+        right_context = re.search(r'\](.+)', note).group(1)
+    return left_context, right_context
+
+def get_sample_entry(note_walker, note, note_info):
+    all_option_same_flag = is_all_option_same(note_info.get('note_options', {}))
+    left_context, right_context = get_note_context(note)
+    data_entry = [
+        note_walker,
+        '',
+        left_context,
+        note_info['note_options']['derge'],
+        note_info['note_options']['chone'],
+        note_info['note_options']['peking'],
+        note_info['note_options']['narthang'],
+        right_context,
+        '',
+        '',
+        '',
+        all_option_same_flag,
+        note_info['count'],
+        note_info['text_id'],
+        ]
+    return data_entry
 
 def generate_csv_report(note_samples):
     output_path = f"./data_samples.csv"
-    header = ["S.no", "Note", "Frequency", "Src Text"]
+    header = ["S.no", 'ཐག་གཅོད།་', "Left context", 'སྡེ།', 'ཅོ།', 'པེ།', 'སྣར།', 'Right context', 'ལེགས་སྦྱར།', 'བརྡ་རྙིང་།', 'བརྡ་གསར།', 'ལྡབ་ལོག།', "Frequency", "Src Text"]
     with open(output_path, "w", encoding="UTF8") as f:
         writer = csv.writer(f)
         writer.writerow(header)
         for note_walker,(note, note_info) in enumerate(note_samples.items(),1):
-            data = [note_walker, note, note_info['count'], note_info['text_id']]
+            data = get_sample_entry(note_walker, note, note_info)
             writer.writerow(data)
 
 
