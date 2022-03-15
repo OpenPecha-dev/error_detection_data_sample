ae7e0ac48fd98b35711011daeb861e6a8b0ea58f
kaldan007 <kaldantenzin@gmail>
2022-03-15 17:15:24 +0530

    batch 2 included

--- get_collated_text.py
+++ get_collated_text.py
@@ -12,7 +12,7 @@ def get_collated_text(text_id):
     
 
 if __name__ == "__main__":
-    collated_text = Path('./collated_text_list.txt').read_text(encoding='utf-8')
+    collated_text = Path('./batch_2_collated_text.txt').read_text(encoding='utf-8')
     collated_text_ids = collated_text.splitlines()
     for collated_text_id in collated_text_ids[11:]:
         collated_text = get_collated_text(collated_text_id)
