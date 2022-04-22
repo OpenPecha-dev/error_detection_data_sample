5cc29437c9ffcffa8cfd214068405e8fcb0f9c43
kaldan007 <kaldantenzin@gmail.com>
2022-04-22 15:05:57 +0530

    collated text updated

--- get_collated_text.py
+++ get_collated_text.py
@@ -13,8 +13,13 @@ def get_collated_text(text_id):
 
 if __name__ == "__main__":
     collated_text = Path('./collated_text_list.txt').read_text(encoding='utf-8')
+    collated_text_list = list(Path('./collated_text').iterdir())
+    collated_text_list.sort()
+    collated_text_list = [text_id.stem[:-5] for text_id in collated_text_list]
     collated_text_ids = collated_text.splitlines()
     for collated_text_id in collated_text_ids:
+        if collated_text_id in collated_text_list:
+            continue
         collated_text = get_collated_text(collated_text_id)
         while not collated_text:
             collated_text = get_collated_text(collated_text_id)
